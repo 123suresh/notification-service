58b1a8389a76110e555854bc8b7f7a4db9eb3e62
sthapa <sureshthapa409@gmail.com>
2023-12-10 14:24:02 +0545

    Create notify post request done

--- internal/controller/routes.go
+++ internal/controller/routes.go
@@ -1,1 +1,33 @@
 package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func (ctl *Controller) Routes() {
+	ctl.router.Use(CORSMiddleware())
+	ctl.router.GET("/", home)
+	ctl.router.POST("/notification", ctl.CreateNotify)
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-agent-code")
+		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
+func home(c *gin.Context) {
+	logrus.Info("chekcing routes")
+	c.JSON(http.StatusOK, "This is notification server")
+}
